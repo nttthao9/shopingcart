1219fe4fae171f50e5c311cd1f00f42661109bb3
Thu Thao <thuthaont0999@gmail.com>
2021-10-07 16:40:26 +0700

    upload code day08

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 const listItem = [
   {
-    id: 1,
+    id: 0,
     name: 'Brown and white cake',
     image:
       'https://images.unsplash.com/photo-1628024634012-39a1bdd0741d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
     price: 10,
   },
   {
-    id: 2,
+    id: 1,
     name: 'Toast',
     image:
       'https://images.unsplash.com/photo-1633104060360-914c5aecb9aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
     price: 8,
   },
   {
-    id: 3,
+    id: 2,
     name: 'Baked cakes',
     image:
       'https://images.unsplash.com/photo-1633104061003-a391ef6716e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
     price: 20,
   },
   {
-    id: 4,
+    id: 3,
     name: 'Brown and white chocolate',
     image:
       'https://images.unsplash.com/photo-1624001877690-922b1e243939?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
@@ -54,7 +54,7 @@ productList.innerHTML = listItem
   .map((item) => renderList(item.image, item.name, item.price))
   .join('');
 
-function renderCartList(image, name, price, quantity = 0) {
+function renderCartList(image, name, price, quantity, id) {
   return `<div class="cart-item">
   <img src=${image} alt="" class="cart-image">
   <div class="cart-desc">
@@ -63,10 +63,10 @@ function renderCartList(image, name, price, quantity = 0) {
     <div class="change-cart">
       <i class="fa fa-plus add-item"></i>
       <span class="count-item">${quantity}</span>
-      <i class="fa fa-minus minus-item"></i>
+      <i class="fa fa-minus minus-item" data-minus = "${id}"></i>
     </div>
   </div>
-  <i class="fa fa-trash trash-icon"></i>
+  <i class="fa fa-trash trash-icon " data-id ="${id}"></i>
 </div>`;
 }
 
@@ -75,26 +75,34 @@ const countQuantity = document.querySelectorAll('.count');
 const addBtn = document.querySelectorAll('.add');
 const minusBtn = document.querySelectorAll('.minus');
 const totalAll = document.querySelector('.total-all');
-let value;
 
-[...addBtn].forEach((item) =>
+let value;
+let localData =
+  localStorage.length !== 0 ? JSON.parse(localStorage.getItem('cartList')) : [];
+cartList = localData;
+cart.innerHTML = localData
+  .map((item) =>
+    renderCartList(item.image, item.name, item.price, item.quantity, item.id)
+  )
+  .join('');
+cartList = localData;
+deleteItem();
+addInCart();
+minusInCart();
+price();
+[...addBtn].forEach((item, i) =>
   item.addEventListener('click', function (e) {
-    value = +e.target.nextElementSibling.textContent;
-    value++;
-    e.target.nextElementSibling.textContent = value;
+    +countQuantity[i].textContent++;
   })
 );
-[...minusBtn].forEach((item) =>
+[...minusBtn].forEach((item, i) =>
   item.addEventListener('click', function (e) {
-    value = e.target.previousElementSibling.textContent;
-    +value--;
-    if (+e.target.previousElementSibling.textContent <= 0) {
-      return 0;
+    +countQuantity[i].textContent--;
+    if (+countQuantity[i].textContent <= 0) {
+      countQuantity[i].textContent = 0;
     }
-    e.target.previousElementSibling.textContent = value;
   })
 );
-let totalPrice = 0;
 
 [...btnOrder].forEach((item, index) =>
   item.addEventListener('click', () => {
@@ -105,20 +113,30 @@ let totalPrice = 0;
       if (!cartList.length) {
         showIncart(item, j, countItem);
       } else {
-        for (let i = 0; i < cartList.length; i++) {
-          if (item.id == cartList[i].id) {
-            alert(
-              'you already chosen this item! you can change your quantity in your cart'
-            );
-            countQuantity[j].textContent = 0;
-            return;
-          }
+        const index = cartList.findIndex((key) => key.id === item.id);
+        if (index === -1) {
+          showIncart(item, j, countItem);
+        } else {
+          cartList[index].quantity += countItem;
+          cartList[index].total =
+            +cartList[index].quantity * +cartList[index].price;
+          countQuantity[j].textContent = 0;
+          localStorage.setItem('cartList', JSON.stringify(cartList));
+          cart.innerHTML = cartList
+            .map((item) =>
+              renderCartList(
+                item.image,
+                item.name,
+                item.price,
+                item.quantity,
+                item.id
+              )
+            )
+            .join('');
         }
-        showIncart(item, j, countItem);
       }
       addInCart();
       minusInCart();
-      deleteItem();
       price();
     } else {
       alert('you must chose quantity');
@@ -129,15 +147,28 @@ let totalPrice = 0;
 function showIncart(item, i, countItem) {
   item.quantity = countItem;
   item.total = countItem * item.price;
-
   cartList.push(item);
+  localStorage.setItem('cartList', JSON.stringify(cartList));
   cart.innerHTML = cartList
     .map((item) =>
-      renderCartList(item.image, item.name, item.price, item.quantity)
+      renderCartList(item.image, item.name, item.price, item.quantity, item.id)
     )
     .join('');
-  console.log(cartList);
+  deleteItem();
   countQuantity[i].textContent = 0;
+}
+function deleteItem() {
+  const deleteIcon = document.querySelectorAll('.trash-icon');
+  deleteIcon.forEach((item) =>
+    item.addEventListener('click', (e) => {
+      const id = e.target.dataset.id;
+      const index = cartList.findIndex((item) => item.id === +id);
+      cartList.splice(index, 1);
+      cartList = localStorage.setItem('cartList', JSON.stringify(cartList));
+      price();
+      e.target.parentNode.remove();
+    })
+  );
 }
 
 function addInCart() {
@@ -148,10 +179,9 @@ function addInCart() {
       value++;
       e.target.nextElementSibling.textContent = value;
       cartList[index].quantity = value;
-      console.log(value);
       cartList[index].total = value * cartList[index].price;
+      localStorage.setItem('cartList', JSON.stringify(cartList));
       price();
-      // console.log('check1');
     })
   );
 }
@@ -159,43 +189,28 @@ function minusInCart() {
   const minusItem = document.querySelectorAll('.minus-item');
   [...minusItem].forEach((item, index) =>
     item.addEventListener('click', (e) => {
-      value = e.target.previousElementSibling.textContent;
-      +value--;
-      if (value < 0) {
+      value = +e.target.previousElementSibling.textContent;
+      value--;
+      e.target.previousElementSibling.textContent = value;
+      if (value <= 0) {
         e.target.previousElementSibling.textContent = 0;
         return;
       }
-      e.target.previousElementSibling.textContent = value;
       cartList[index].quantity = value;
       cartList[index].total = value * cartList[index].price;
+      localStorage.setItem('cartList', JSON.stringify(cartList));
       price();
     })
   );
 }
-
 function price() {
+  cartList =
+    localStorage.length !== 0
+      ? JSON.parse(localStorage.getItem('cartList'))
+      : [];
   totalAll.textContent =
     cartList.reduce((previous, item) => previous + item.total, 0) + '$';
 }
-
-function deleteItem() {
-  const deleteIcon = document.querySelectorAll('.trash-icon');
-  deleteIcon.forEach((item, i) =>
-    item.addEventListener('click', (e) => {
-      let itemdelete = cartList[i];
-      e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-      //doan nay co bug :<
-      cartList = cartList.filter(
-        (item) => item.id !== itemdelete.id,
-        'checkkkk'
-      );
-      console.log(cartList);
-      price();
-    })
-  );
-  price();
-}
-
 shopingIcon.addEventListener('click', function () {
   cartOrder.classList.toggle('active');
 });
